ddad6deb8da7e527f111d3aede53e573b9511dcf
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-29 13:32:05 +0530

    Almost

--- app/src/main/java/com/piyushmaheswari/imageloader/Adapter/LazyAdapter.java
+++ app/src/main/java/com/piyushmaheswari/imageloader/Adapter/LazyAdapter.java
@@ -1,4 +1,54 @@
 package com.piyushmaheswari.imageloader.Adapter;
 
-public class LazyAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.piyushmaheswari.imageloader.Model.ImageLoader;
+import com.piyushmaheswari.imageloader.R;
+
+public class LazyAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private String[] data;
+    private static LayoutInflater inflater=null;
+    public ImageLoader imageLoader;
+
+    public LazyAdapter(Activity a, String[] d) {
+        activity = a;
+        data=d;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        imageLoader=new ImageLoader(activity.getApplicationContext());
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View vi=view;
+        if(view==null)
+            vi = inflater.inflate(R.layout.list_item, null);
+
+        ImageView image=(ImageView)vi.findViewById(R.id.image);
+        imageLoader.DisplayImage(data[i], image);
+        return vi;
+    }
 }
